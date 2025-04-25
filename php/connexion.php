7230a5f15af98a739ac1b3f0d44b41f1349fda31
DivinKimia <dkimpolokimia@edouardgand.fr>
2025-04-25 11:32:49 +0200

    Connexion base donnees

--- php/connexion.php
+++ php/connexion.php
@@ -2,14 +2,14 @@
 
 // Informations de connexion
 $servername = "localhost"; // Adresse serveur
-$username = "votre_nom_utilisateur"; 
-$password = "votre_mot_de_passe"; 
+$username = ""; 
+$password = ""; 
 $dbname = "Formula_One_fantasy_league"; // Nom de base de données
 
-// Création de la connexion
+// Crée la connexion
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Vérification de la connexion
+// Vérification connexion
 if ($conn->connect_error) {
     die("Échec de la connexion: " . $conn->connect_error);
 }
